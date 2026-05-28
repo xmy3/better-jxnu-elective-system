0addf2442732dac372ba2438b648b8c4e797a1b3
guiguisocute <guiguisocute@linux.do>
2026-05-28 12:40:37 +0800

    fix

--- src/components/CourseTable.tsx
+++ src/components/CourseTable.tsx
@@ -238,7 +238,7 @@ export function CourseTable({
               <svg className="w-3.5 h-3.5 shrink-0" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                 <path fillRule="evenodd" clipRule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5z" />
               </svg>
-              <span className="truncate">此表数据仅供功能测试，本学期开课安排暂未发布，请注意</span>
+              <span className="truncate">此表数据仅供功能测试，{selectedSemester}开课安排暂未发布，请注意</span>
             </div>
           )}
           {onChangeSemester && (
